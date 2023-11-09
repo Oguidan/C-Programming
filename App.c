55baaf841622456dfe72d47b00c4e408a6871608
Michael Oguidan <michaeloguidan@gmail.com>
2023-11-09 20:30:45 +0000

    N dimentional array

--- App.c
+++ App.c
@@ -107,5 +107,15 @@ luckyNumbers[0] = 90;
 printf("%d \n", luckyNumbers[0]);
 printf("%d \n", luckyNumbers[1]);
 
+// N Dimensional Arrays
+
+ int numberGrid[2][3];
+// int numberGrid[2][3] = {{1, 2, 3}, {4, 5, 6}};
+printf("%d \n",numberGrid[0][1]);
+numberGrid[0][1] = 99;
+
+printf("%d \n",numberGrid[0][0]);
+printf("%d \n",numberGrid[0][1]);
+
   return 0;
 }
