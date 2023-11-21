a26a254c2a317a55c74979f10273b107ee054d52
Michael Oguidan <michaeloguidan@gmail.com>
2023-11-21 21:03:29 +0000

    While loops

--- App.c
+++ App.c
@@ -161,6 +161,22 @@ default:
     printf("Invalid grade\n");
 }
 
+// WHILE LOOPS
+int index = 6;
+// while (index <= 5)
+// {
+    // printf("%d\n", index);
+    // index++;
+// }
+
+ do
+ {
+  printf("%d\n", index);
+  index++;
+ } while (index <= 5);
+
+
+
   return 0;
 }
 
